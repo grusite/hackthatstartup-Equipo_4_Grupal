8c5d80dc33be58d1e255e7e67ed23b710cf9aa24
Jorge Martín <grusite@gmail.com>
2021-01-24 12:51:27 +0100

    added auth in user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const { pick } = require('lodash')
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 12
 
 const schema = mongoose.Schema(
     {
@@ -26,9 +29,47 @@ const schema = mongoose.Schema(
             jobDescription: String,
             jobStartYear: Number,
             jobEndYear: Number,
-        }]
+        }],
+        provider: {
+            gitHub: {
+                id: String,
+            },
+            traditional: {
+                password: {
+                    type: String,
+                    requird: true,
+                },
+            },
+        },
     },
     { timestamps: true }
 )
+
+schema.methods.toPublic = function () {
+    return pick(this, ['name', 'email'])
+}
+
+schema.methods.hasProvider = function (type) {
+    const obj = this.toObject()
+    return obj.provider && obj.provider[type]
+}
+
+schema.pre('save', async function () {
+    // only hash the password if it has been modified (or is new)
+    if (!this.isModified('provider.traditional.password')) return
+
+    // generate a salt
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR)
+
+    // hash the password using our new salt
+    const hash = await bcrypt.hash(this.provider.traditional.password, salt)
+
+    // override the cleartext password with the hashed one
+    this.provider.traditional.password = hash
+})
+
+schema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.provider.traditional.password)
+}
 
 module.exports = mongoose.model('user', schema)
