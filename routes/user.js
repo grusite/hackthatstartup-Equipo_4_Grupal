2179e17b4029b040c4c4ed020253254bb4ff7e22
Jorge Martín <grusite@gmail.com>
2021-01-24 12:52:03 +0100

    added auth user routes

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,11 @@ const userController = require('../controllers/userController');
 
 expressDeliver(router);
 
+//Auth routes
+router.post('/login', userController.login);
+router.post('/register', userController.register);
+
+//CRUD routes
 router.get('/', userController.listUsers);
 router.get('/:userEmail', userController.readUser);
 router.post('/', userController.addUser);
